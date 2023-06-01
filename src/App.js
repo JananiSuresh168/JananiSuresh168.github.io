c47068dd5ec1fd1d017779d75ff88ac88d390b37
JananiSuresh168 <janani.suresh.0801@gmail.com>
2023-06-01 19:44:30 -0400

    About Me Section

--- src/App.js
+++ src/App.js
@@ -57,7 +57,7 @@ const App = () => {
         contribute to the success of development teams.
       </p>
     <div className="resume-container">
-      <a href="/resumes/resume.pdf" download>
+      <a href="resume.pdf" download>
         Download Resume
       </a>
     </div>
